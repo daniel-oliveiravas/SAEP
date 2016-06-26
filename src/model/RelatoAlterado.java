402441985ca6d75dd94bf9e52335f6589e7fac25
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-06-26 20:07:48 -0300

    Adicionado repository para RelatoAlteardo

--- src/model/RelatoAlterado.java
+++ src/model/RelatoAlterado.java
@@ -2,6 +2,7 @@ package model;
 
 public class RelatoAlterado {
 
+    String identificador;
     String justificativaDaAlteracao;
     Relato relatoModificado;
 
---
+++ src/persistencia/RelatoAlteradoRepository.java
@@ -0,0 +1,34 @@
+package persistencia;
+
+import model.Atributo;
+import model.Relato;
+import model.RelatoAlterado;
+
+/*
+* Repositório responsável pela abstração da persistência
+* do objeto Atributo
+* */
+public interface RelatoAlteradoRepository {
+
+    /*
+    * Salva um RelatoAlterado
+    *
+    * @param RelatoAlterado relatoAlterado - O relatoAlterado a ser persistido
+    * */
+    void salvarRelatoAlterado(RelatoAlterado relatoAlterado);
+
+    /*
+    * Remove um RelatoAlterado
+    *
+    * @param String identificador - O identificador do relatoAlterado a ser removido
+    * */
+    void removerRelatoAlterado(String identificador);
+
+    /*
+    * Busca um relatoAlterado pelo identificador
+    *
+    * @param String identificador - O identificador do relatoAlterado a ser buscado
+    * */
+    RelatoAlterado findByIdentificador(String identificador);
+
+}
